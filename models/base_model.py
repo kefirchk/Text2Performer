ec00b780fffc3e5663361c91c02137b28195fa60
kefirchk <prostolex2004@mail.ru>
2025-05-29 10:45:18 +0300

    Adaptated app for any device

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel():
 
     def __init__(self, opt):
         self.opt = opt
-        self.device = torch.device('cuda' if opt['num_gpu'] != 0 else 'cpu')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.is_train = opt['is_train']
 
     def feed_data(self, data):
